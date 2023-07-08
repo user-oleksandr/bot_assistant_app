72ad9a90e6f6a392c4cf7f98f71b004ba44fd5c2
Oleksandr Anastasiiev <name.oleksandr@gmail.com>
2023-07-08 19:02:36 +0300

    create navigations

--- src/components/navigations/Navigation.js
+++ src/components/navigations/Navigation.js
@@ -3,46 +3,32 @@ import {NavLink} from 'react-router-dom';
 
 const Navigation = () => {
     return (
-        <nav className="navbar navbar-dark navigations fixed-top bg-secondary navbar-expand-lg">
+        <nav className="navbar bg-secondary fixed-top navbar-dark">
             <div className="container-fluid">
-                <button className="navbar-toggler text-light"
-                    type="button"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
+                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
+                        aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div
-                    className="offcanvas offcanvas-end bg-secondary rounded"
-                    tabIndex="-1"
-                    id="navbarNav"
-                    aria-labelledby="navbarNavLabel"
-                    style={{width: '20em'}}
-                >
-                    <div className="offcanvas-header ">
-                        <h5 className="offcanvas-title text-light" id="navbarNavLabel">
-                            Personal Assistant
-                        </h5>
-                        <button
-                            type="button"
-                            className="btn-close btn-close-white"
-                            data-bs-dismiss="offcanvas"
-                            aria-label="Close"
-                        ></button>
+                <a className="navbar-brand">Personal assistant</a>
+                <div className="offcanvas offcanvas-start bg-secondary rounded shadow" tabIndex="-1"
+                     id="offcanvasNavbar"
+                     aria-labelledby="offcanvasNavbarLabel" style={{width: '275px'}}>
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title text-light" id="offcanvasNavbarLabel">Personal assistant</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
                     </div>
-                    <div className="offcanvas-body justify-content-center">
-                        <ul className="navbar-nav">
+                    <div className="offcanvas-body">
+                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                             <li className="nav-item">
-                                <NavLink className="nav-link text-light" to="/" activeClassName="active">
+                                <NavLink className="nav-link" to="/" activeClassName="active">
                                     Home
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link text-light" to="/about" activeClassName="active">
-                                   About
+                                <NavLink className="nav-link" to="/about" activeClassName="active">
+                                    About
                                 </NavLink>
                             </li>
                         </ul>
@@ -50,7 +36,6 @@ const Navigation = () => {
                 </div>
             </div>
         </nav>
-
     );
 };
 
